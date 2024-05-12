3d4a8e6038dc673f73b83ba57dc4e18c1a1dedba
Gray Sinclair <gray.sinclair@icloud.com>
2024-05-12 05:00:05 -0500

    Changes: ar-setup.vb

--- ar-setup.vb
+++ ar-setup.vb
@@ -21,3 +21,21 @@ Sub AR_Setup()
 '        With Columns("A:AG")
 '            .ClearFormats
 '        End With
+
+        ' COLUMN NAMES
+        .Cells(1, 1).Value = "Unit"
+        .Cells(1, 9).Value = "Last Bill Ref"
+        .Cells(1, 11).Value = "OdyNum"
+        'Text to col which helps make data consistent and removes white spaces.
+        Dim columnsToFormat As Variant
+        Dim col As Variant
+ 
+        columnsToFormat = Array("D", "F", "G", "H", "I", "J", "K", "W", "X", "AF")
+       
+        For Each col In columnsToFormat
+            With .Columns(col)
+                .TextToColumns Destination:=.Cells(1, 1), DataType:=xlDelimited, _
+                    TextQualifier:=xlDoubleQuote, ConsecutiveDelimiter:=False, _
+                    Tab:=False, Semicolon:=False, Comma:=False, Space:=False, Other:=False
+            End With
+        Next col
