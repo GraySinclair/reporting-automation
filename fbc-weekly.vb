1dfb7d1896d783bdc3d1e6b314ebd3d369ac2ccd
Gray Sinclair <gray.sinclair@icloud.com>
2024-08-06 18:35:25 -0500

    Changes: fbc-weekly.vb

--- fbc-weekly.vb
+++ fbc-weekly.vb
@@ -7,4 +7,23 @@ Sub FBC_Report()
     Dim filterCriteria As String
     Dim copyRange As Range
 
-'TODO: Set Refs
+    ' References
+    Set wb = ActiveWorkbook
+    Set wsFBCData = Nothing
+    On Error Resume Next
+    Set wsFBCData = wb.Sheets("FBC Data")
+    On Error GoTo 0
+    Set wsFBCReport = Nothing
+    On Error Resume Next
+    Set wsFBCReport = wb.Sheets("FBC Report")
+    On Error GoTo 0
+    Set wsItems = Nothing
+    On Error Resume Next
+    Set wsItems = wb.Worksheets("ITEMS")
+    On Error GoTo 0
+
+    ' Create worksheet "FBC Data" if it doesn't exist
+    If wsFBCData Is Nothing Then
+        Set wsFBCData = wb.Sheets.Add(After:=wb.Sheets(wb.Sheets.Count))
+        wsFBCData.Name = "FBC Data"
+    End If
