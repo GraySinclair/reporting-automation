ced21466a897344c9004f186efb4cac387a65841
Gray Sinclair <gray.sinclair@icloud.com>
2024-08-07 21:17:23 -0500

    Changes: fbc-weekly.vb

--- fbc-weekly.vb
+++ fbc-weekly.vb
@@ -27,3 +27,20 @@ Sub FBC_Report()
         Set wsFBCData = wb.Sheets.Add(After:=wb.Sheets(wb.Sheets.Count))
         wsFBCData.Name = "FBC Data"
     End If
+
+    ' Create worksheet "FBC Report" if it doesn't exist
+    If wsFBCReport Is Nothing Then
+        Set wsFBCReport = wb.Sheets.Add(After:=wb.Sheets(wb.Sheets.Count))
+        wsFBCReport.Name = "FBC Report"
+    End If
+
+    ' Filter the range in ITEMS sheet to pull filtered FBC data
+    On Error Resume Next
+    ActiveSheet.ShowAllData
+    With wsItems.Range("$A$1:$AG$9999") 'TODO: change this to a from start to "lastrow" to improve code functionality
+        .AutoFilter Field:=2, Criteria1:="XZ55FFT"
+        ' Check if there are visible cells in the filtered range
+        On Error Resume Next
+        Set copyRange = .SpecialCells(xlCellTypeVisible) 'TODO: change from visible cells to requirement based on filters
+        On Error GoTo 0
+    End With
