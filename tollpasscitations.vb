450d53816917879b6966c3ddf4848fc10bb898d7
Gray Sinclair <gray.sinclair@icloud.com>
2024-12-22 18:28:51 -0600

    Changes: tollpasscitations.vb

---
+++ tollpasscitations.vb
@@ -0,0 +1,16 @@
+Sub CorpBillCitationAdminFees()
+    'worksheets
+    Dim tssfee As Worksheet
+    Dim tsstotal As Worksheet
+    Dim access As Worksheet
+    Dim historic As Worksheet
+   
+    
+    ' PreWork ----------------------------------------
+    Application.DisplayAlerts = False
+    Application.ScreenUpdating = False
+    Application.Calculation = xlCalculationManual ' Turn off automatic calculation
+    Application.EnableEvents = False ' Disable events temporarily
+ 
+
+End Sub
