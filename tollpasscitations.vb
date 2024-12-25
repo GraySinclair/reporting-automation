2b1b4686eb4dd4d632ddc5ddf3c29f9115591694
Gray Sinclair <gray.sinclair@icloud.com>
2024-12-24 22:08:55 -0600

    Changes: tollpasscitations.vb

--- tollpasscitations.vb
+++ tollpasscitations.vb
@@ -21,4 +21,20 @@ Sub CorpBillCitationAdminFees()
     RemoveBlankColumns tssfee, 23
     RemoveBlankColumns tsstotal, 19
    
+    ' deleting blank columns if they exist in tssfees and tsstotal---------------------------------
+    'Dim col As Long
+    ' Loop through columns from X to A (backward to avoid shifting)
+    ' deleting blank columns in tssfee
+'    For col = 23 To 1 Step -1
+'        If WorksheetFunction.CountA(tssfee.Columns(col)) = 0 Then
+'            tssfee.Columns(col).Delete
+'        End If
+'    Next col
+   
+    ' deleting blank columns in tsstotal
+    For col = 19 To 1 Step -1
+        If WorksheetFunction.CountA(tsstotal.Columns(col)) = 0 Then
+            tsstotal.Columns(col).Delete
+        End If
+    Next col
 End Sub
